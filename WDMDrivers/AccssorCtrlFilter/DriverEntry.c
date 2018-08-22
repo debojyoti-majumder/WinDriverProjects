759277f017a8583fc493ae4f36519552e839e232
Majumder, Debojyoti <Debojyoti.Majumder@gmail.com>
2018-08-23 01:04:47 +0530

    Added some functions

--- WDMDrivers/AccssorCtrlFilter/DriverEntry.c
+++ WDMDrivers/AccssorCtrlFilter/DriverEntry.c
@@ -1,10 +1,49 @@
 #include <fltKernel.h>
+#include "RegistrationData.h"
 
 DRIVER_INITIALIZE DriverEntry;
 
 #ifdef ALLOC_PRAGMA
 	#pragma alloc_text(INIT, DriverEntry)
 #endif // ALLOC_PRAGMA
+
+
+FLT_PREOP_CALLBACK_STATUS
+SpyPreOperationCallback(
+	_Inout_ PFLT_CALLBACK_DATA Data,
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_Flt_CompletionContext_Outptr_ PVOID *CompletionContext
+) {
+	UNREFERENCED_PARAMETER(Data);
+	UNREFERENCED_PARAMETER(FltObjects);
+	UNREFERENCED_PARAMETER(CompletionContext);
+
+	return FLT_PREOP_SUCCESS_NO_CALLBACK;
+}
+
+FLT_POSTOP_CALLBACK_STATUS
+SpyPostOperationCallback(
+	_Inout_ PFLT_CALLBACK_DATA Data,
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ PVOID CompletionContext,
+	_In_ FLT_POST_OPERATION_FLAGS Flags
+) {
+	UNREFERENCED_PARAMETER(Data);
+	UNREFERENCED_PARAMETER(FltObjects);
+	UNREFERENCED_PARAMETER(CompletionContext);
+	UNREFERENCED_PARAMETER(Flags);
+
+	return FLT_POSTOP_FINISHED_PROCESSING;
+}
+
+CONST FLT_OPERATION_REGISTRATION Callbacks[] = {
+	{ IRP_MJ_CREATE,
+	0,
+	SpyPreOperationCallback,
+	SpyPostOperationCallback },
+	
+	{ IRP_MJ_OPERATION_END }
+};
 
 
 NTSTATUS 
