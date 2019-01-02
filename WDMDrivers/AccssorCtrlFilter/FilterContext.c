4e0005eebc9e79543b298da451812ff981ed5133
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-01-02 15:21:24 +0530

    Added context clean up funcationality

--- WDMDrivers/AccssorCtrlFilter/FilterContext.c
+++ WDMDrivers/AccssorCtrlFilter/FilterContext.c
@@ -5,6 +5,19 @@ FilterContextCleanup(
 	IN PFLT_CONTEXT Context,
 	IN FLT_CONTEXT_TYPE ContextType
 ) {
-	UNREFERENCED_PARAMETER(Context);
-	UNREFERENCED_PARAMETER(ContextType);
+	PCTX_INSTANCE_CONTEXT pInstanceCtx;
+
+	switch (ContextType) {
+		case FLT_INSTANCE_CONTEXT:
+			pInstanceCtx = (PCTX_INSTANCE_CONTEXT)Context;
+			
+			// Perform cleanup
+			if (pInstanceCtx != NULL) {
+				RtlFreeUnicodeString(&pInstanceCtx->VolumeName);
+			}
+			break;
+		
+		default:
+			break;
+	}
 }
