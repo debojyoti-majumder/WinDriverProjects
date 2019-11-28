278c862ae2229d6c524d172d0dd7df4918d350da
debojyotim <debojyotim@vmware.com>
2019-11-28 13:02:37 +0530

    Able to distinguish between

--- WDMDrivers/goEncryptor/main.go
+++ WDMDrivers/goEncryptor/main.go
@@ -5,17 +5,38 @@ import (
 	"os"
 )
 
+func processFile(filePath string) {
+	fmt.Println("Processing files")
+}
+
+func processFolders(folderPath string) {
+	fmt.Println("Processing the folder")
+}
+
 func main() {
 	// Basic command line checking
 	argCount := len(os.Args)
-	if argCount < 3 {
+
+	if argCount < 1 {
 		fmt.Println("Invalid usage")
 		return
-	}
+	} else if argCount == 3 {
+		// Some verbose output
+		keyName := os.Args[1]
+		entityName := os.Args[2]
+		fmt.Println("Encrypt Using key", keyName)
+		fmt.Println("On entity:", entityName)
 
-	// Some verbose output
-	keyName := os.Args[1]
-	entityName := os.Args[2]
-	fmt.Println("Using key", keyName)
-	fmt.Println("On entity:", entityName)
+		fileInformation, err := os.Stat(entityName)
+		if err == nil {
+			if fileInformation.IsDir() {
+				// Processing the folder
+				processFolders(entityName)
+			} else {
+				processFile(entityName)
+			}
+		}
+	} else if argCount == 2 {
+		fmt.Println("Decrypt file content")
+	}
 }
