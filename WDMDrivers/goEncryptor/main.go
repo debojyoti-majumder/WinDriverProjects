4c6b6c43e1eb8494e163325eb4bb69046ab73756
debojyotim <debojyotim@vmware.com>
2019-11-28 12:35:41 +0530

    parsing command line argument

--- WDMDrivers/goEncryptor/main.go
+++ WDMDrivers/goEncryptor/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("This is a very basic encryptor tool")
+	// Basic command line checking
+	argCount := len(os.Args)
+	if argCount < 3 {
+		fmt.Println("Invalid usage")
+		return
+	}
+
+	// Some verbose output
+	keyName := os.Args[1]
+	entityName := os.Args[2]
+	fmt.Println("Using key", keyName)
+	fmt.Println("On entity:", entityName)
 }
